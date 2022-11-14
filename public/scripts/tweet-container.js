2a14e8fa4027bd443fc123bedec0d8b6bfc752a1
Richard Miao <richardmiao1645@gmail.com>
2022-11-13 16:51:00 -0800

    Add tweet container logic

---
+++ public/scripts/tweet-container.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  //Applies a shadow when hovering over tweet container
+  $(".tweet-container").mouseover(function() {
+    $(this).css("box-shadow", "10px 10px #c9cbdf");
+  })
+  $(".tweet-container").mouseout(function(){
+    $(this).css("box-shadow", "none");
+  });
+
+  //Changes color of the tweet reactions when mousing over
+  $(".fa-flag").mouseover(function() {
+    $(this).css("color", "#cc9924");
+  })
+  $(".fa-flag").mouseout(function(){
+    $(this).css("color", "#4055a1");
+  });
+  $(".fa-retweet").mouseover(function() {
+    $(this).css("color", "#cc9924");
+  })
+  $(".fa-retweet").mouseout(function(){
+    $(this).css("color", "#4055a1");
+  });
+  $(".fa-heart").mouseover(function() {
+    $(this).css("color", "#cc9924");
+  })
+  $(".fa-heart").mouseout(function(){
+    $(this).css("color", "#4055a1");
+  });
+});
