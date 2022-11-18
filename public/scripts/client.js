f6bc5587496e0ab7f0e7b604b0d06853008c7a21
Richard Miao <richardmiao1645@gmail.com>
2022-11-17 16:22:03 -0800

    Add Ajax request to load tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 //Turns an object-like set of data to HTML
 const createTweetElement = function(tweetObject) {
   let tweetHTML = `
@@ -44,7 +19,7 @@ const createTweetElement = function(tweetObject) {
         ${tweetObject.content.text}
       </p>
     <footer>
-      <p>${tweetObject.created_at}</p>
+      <p>${timeago.format(tweetObject.created_at)}</p>
       <div class="tweet-container-footer-reactions">
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-retweet"></i>
@@ -69,5 +44,29 @@ const renderTweets = function(tweets) {
 
 $(document).ready(function() {
   //Appends all the tweets from the data to the main container
-  $('.container').append(renderTweets(data)); 
+  /*
+  const getAjaxData = function(res) {
+    $.ajax({
+      url: "http://localhost:8080/tweets",
+      type: 'GET',
+      dataType: 'json', // added data type
+      success: function(res) {
+        console.log(res)
+        $('.container').append(renderTweets(res)); 
+      }
+    });
+  }
+  */
+
+  $.ajax({
+    url: "http://localhost:8080/tweets",
+    type: 'GET',
+    dataType: 'json', // added data type
+    success: function(res) {
+      console.log(res)
+      $('.container').append(renderTweets(res)); 
+    }
+  });
 });
+
+modules.exports = { getAjaxData }
--- public/scripts/new-tweet.js
+++ public/scripts/new-tweet.js
@@ -1,14 +1,27 @@
 //Handles script when the user submits a tweet
 $(document).ready(function() {
+  //Changes color of the "Tweet" button on mouseover
+  $("#tweet-button").mouseover(function() {
+    $(this).css("background-color", "#829bef");
+  })
+  $("#tweet-button").mouseout(function(){
+    $(this).css("background-color", "#4056a1");
+  });
+
   $("#tweet-post").on("submit", function(event) {
     event.preventDefault() //Prevents page refresh
-    let tweetContent = $(this).serialize()
-    console.log(tweetContent)
+    let tweetContent = $(this).serialize();
+    if ($("#tweet-text").val().length < 1) {
+      alert("Please enter a valid tweet.");
+    
+    } else if ($("#tweet-text").val().length > 140) {
+      alert("Your tweet is too long.");
+    
+    } else {
     //Ajax Post request format: $.post(URL,data,callback); 
     $.post("http://localhost:8080/tweets", //URL
-      tweetContent, //Data
-      (data, status) => { //Callback
-        alert("Data: " + data + "\nStatus: " + status);
-      })
+    tweetContent //Data
+    )}  
+    
   });
 });
